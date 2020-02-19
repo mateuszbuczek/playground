6c617cb941b5cb3f5dace24b58de43794330b268
Mateusz Buczek <mateuszbuczek.128@gmail.com>
2020-02-19 22:10:25 +0100

    initial

---
+++ angular-apps/wsearch/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {WikipediaService} from './wikipedia.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private wikipedia: WikipediaService) {
+  }
+
+  onTerm(term: string) {
+    const result = this.wikipedia.search(term);
+  }
+}
---
+++ angular-apps/wsearch/src/app/wikipedia.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WikipediaService {
+
+  constructor() { }
+
+  search(term: string) {
+    return 'search result';
+  }
+}
