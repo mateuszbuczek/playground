44800e8d4f91ff3c82e9f46f91f664f654539daf
Mateusz Buczek <mateuszbuczek.128@gmail.com>
2020-02-21 21:05:44 +0100

    call for wikipedia api

--- angular-apps/wsearch/src/app/app.component.ts
+++ angular-apps/wsearch/src/app/app.component.ts
@@ -7,11 +7,14 @@ import {WikipediaService} from './wikipedia.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pages = [];
 
   constructor(private wikipedia: WikipediaService) {
   }
 
   onTerm(term: string) {
-    const result = this.wikipedia.search(term);
+    this.wikipedia.search(term).subscribe((res: any) => {
+      this.pages = res.query.search;
+    });
   }
 }
--- angular-apps/wsearch/src/app/wikipedia.service.ts
+++ angular-apps/wsearch/src/app/wikipedia.service.ts
@@ -1,13 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WikipediaService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
 
   search(term: string) {
-    return 'search result';
+    return this.http.get('https://en.wikipedia.org/w/api.php', {
+      params: {
+        action: 'query',
+        format: 'json',
+        list: 'search',
+        utf8: '1',
+        srsearch: term,
+        origin: '*'
+      }
+    });
   }
 }
