11633570926b3b9f19d77a8eee83c944a6acb1c1
mateusz <hateml.dev@gmail.com>
2020-04-09 20:25:05 +0200

    reduce by key example

--- apache-spark/starter/src/main/java/Main.java
+++ apache-spark/starter/src/main/java/Main.java
@@ -1,7 +1,6 @@
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
@@ -12,21 +11,25 @@ public class Main {
     public static void main(String[] args) {
         System.setProperty("hadoop.home.dir", "C:\\workspace");
 
-        ArrayList<Integer> data = new ArrayList<>();
-        data.add(35);
-        data.add(213);
-        data.add(9);
-
         Logger.getLogger("org.apache").setLevel(Level.WARN);
 
         SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]").set("spark.driver.host", "127.0.0.1");
         JavaSparkContext sparkContext = new JavaSparkContext(conf);
 
-        JavaRDD<Integer> rdd = sparkContext.parallelize(data);
-        JavaRDD<Double> map = rdd.map(Math::sqrt);
-
-        JavaRDD<Tuple2<Integer, Double>> sqrtRdd = rdd.map(value -> new Tuple2<>(value, Math.sqrt(value)));
+        reduceByKeyExample(sparkContext);
 
         sparkContext.close();
+    }
+
+    private static void reduceByKeyExample(JavaSparkContext sparkContext) {
+        ArrayList<String> data = new ArrayList<>();
+        data.add("WARN: 4 September 0405");
+        data.add("ERROR: 2 March 2021");
+        data.add("WARN: 4 March 2021");
+
+        sparkContext.parallelize(data)
+                .mapToPair(value -> new Tuple2<>(value.split(":")[0], 1L))
+                .reduceByKey(Long::sum)
+                .foreach(tuple -> System.out.println(tuple._1 + " has " + tuple._2));
     }
 }
