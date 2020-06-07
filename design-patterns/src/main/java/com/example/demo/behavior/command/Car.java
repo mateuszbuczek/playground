429931b2a78bef9dd06b632268b7992f765e2434
mateusz <hateml.dev@gmail.com>
2020-06-07 11:51:35 +0200

    command pattern

---
+++ design-patterns/src/main/java/com/example/demo/behavior/command/Car.java
@@ -0,0 +1,12 @@
+package com.example.demo.behavior.command;
+
+public class Car {
+
+    public void move() {
+        System.out.println("Car is moving");
+    }
+
+    public void stop() {
+        System.out.println("Car is stopped");
+    }
+}
---
+++ design-patterns/src/main/java/com/example/demo/behavior/command/RemoteControl.java
@@ -0,0 +1,21 @@
+package com.example.demo.behavior.command;
+
+public class RemoteControl {
+    CommandBase onCommand, offCommand, undoCommand;
+
+    public void onButtonPressed(CommandBase onCommand) {
+        this.onCommand = onCommand;
+        onCommand.execute();
+        undoCommand = onCommand;
+    }
+
+    public void offButtonPressed(CommandBase offCommand) {
+        this.offCommand = offCommand;
+        offCommand.execute();
+        undoCommand = offCommand;
+    }
+
+    public void undoButtonPressed() {
+        undoCommand.undo();
+    }
+}
