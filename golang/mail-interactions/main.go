48ec53cd382f5d696250292c173b8598a2dab734
mateusz <hateml.dev@gmail.com>
2020-08-28 10:50:10 +0200

    mail interactions

---
+++ golang/mail-interactions/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/mateuszbuczek/playground/golang/mail-interactions/email"
+	"log"
+	"net/mail"
+	"net/smtp"
+)
+
+func main() {
+	// compose the message
+	m := email.NewMessage("Hi", "this is the body")
+	m.From = mail.Address{Name: "From", Address: "from@example.com"}
+	m.AddTo(mail.Address{Name: "someToName", Address: "to@example.com"})
+	m.AddCc(mail.Address{Name: "someCcName", Address: "cc@example.com"})
+	m.AddBcc(mail.Address{Name: "someBccName", Address: "bcc@example.com"})
+
+	// add attachments
+	if err := m.Attach("email/email.go"); err != nil {
+		log.Fatal(err)
+	}
+
+	// add headers
+	m.AddHeader("X-CUSTOMER-id", "xxxxx")
+
+	// send it
+	auth := smtp.PlainAuth("", "from@example.com", "pwd", "smtp.zoho.com")
+	if err := email.Send("smtp.zoho.com:587", auth, m); err != nil {
+		log.Fatal(err)
+	}
+}
