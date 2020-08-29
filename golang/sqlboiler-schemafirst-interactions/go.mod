828001c426b208eefe6cf9eadaa0a93ef024655e
mateusz <hateml.dev@gmail.com>
2020-08-29 11:48:41 +0200

    sqlboiler interactions

---
+++ golang/sqlboiler-schemafirst-interactions/go.mod
@@ -0,0 +1,10 @@
+module github.com/mateuszbuczek/playground/golang/sqlboiler-schemafirst-interactions
+
+go 1.13
+
+require (
+	github.com/friendsofgo/errors v0.9.2
+	github.com/lib/pq v1.2.1-0.20191011153232-f91d3411e481
+	github.com/volatiletech/sqlboiler/v4 v4.2.0
+	github.com/volatiletech/strmangle v0.0.1
+)
---
+++ golang/sqlboiler-schemafirst-interactions/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/mateuszbuczek/playground/golang/sqlboiler-schemafirst-interactions/models"
+	"github.com/volatiletech/sqlboiler/v4/boil"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "user"
+	password = "password"
+	dbname   = "postgres"
+)
+
+//go:generate sqlboiler psql
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, _ := sql.Open("postgres", psqlInfo)
+	ctx := context.Background()
+
+	pilots, _ := models.Pilots().All(ctx, db)
+
+	for _, p := range pilots {
+		fmt.Printf("p: %+v\n", p)
+	}
+
+	jets, _ := models.Jets().All(ctx, db)
+	for _, j := range jets {
+		fmt.Printf("j1: %+v\n", j)
+	}
+
+	jets, _ = models.Jets(qm.Where("color=?", "red")).All(ctx, db)
+	for _, j := range jets {
+		fmt.Printf("j2: %+v\n", j)
+	}
+
+	pilots, _ = models.Pilots(qm.Limit(1), qm.Offset(0)).All(ctx, db)
+	for _, p := range pilots {
+		fmt.Printf("p2: %+v\n", p)
+	}
+
+	p := models.Pilot{Name: "TOMEK"}
+	_ = p.Insert(ctx, db, boil.Infer())
+}
