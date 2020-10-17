2bb7c97e3b471fba02f99c9b698bf53da18923a3
mateuszbuczek <mateuszbuczek.128@gmail.com>
2020-10-17 21:38:40 +0200

    retry & circuit breaker & bulkhead

--- spring-boot-apps/resillience4j/src/main/java/com/example/demo/Application.java
+++ spring-boot-apps/resillience4j/src/main/java/com/example/demo/Application.java
@@ -1,7 +1,17 @@
 package com.example.demo;
 
+
+import java.net.URI;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.permanentRedirect;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +20,9 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    RouterFunction<ServerResponse> redirectRoot() {
+        return route(GET("/"),
+                req -> permanentRedirect(URI.create("/actuator")).build());
+    }
 }
